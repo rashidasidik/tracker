94c59c313a2c9f9f0d3f9ff55121de6914143e68
Rash <rashidasidik914@gmail.com>
2021-03-24 03:41:13 -0700

    added some files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import  './App.css';
+import Header from './components/Header';
+import Stats from "./components/Stats"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header/>
+    <h1>GLOBEL SUMMARY</h1>
+    <Stats url="https://covid19.mathdro.id/api"/>   
+  </div>
+
   );
+
 }
+
 
 export default App;
